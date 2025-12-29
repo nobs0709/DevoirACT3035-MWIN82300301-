3bcca74081dd4da4c3977d025a276bdc4495a071
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 05:04:53 -0500

    Mesure de dispersion du montant de reclamation

--- src/03_exploration.R
+++ src/03_exploration.R
@@ -5,5 +5,9 @@ library (dplyr)
 data_transf <- read.csv("data/processed/insurance_final.csv")
 
 # Statistiques descriptives
+
 #1. Statistique de base
 summary (data_transf)
+#2.Mesures de dispersion pour le montant des réclamations
+mean_reclam <- mean(data_transf$total_claim_amount)
+sd_reclam <- sd(data_transf$total_claim_amount)
