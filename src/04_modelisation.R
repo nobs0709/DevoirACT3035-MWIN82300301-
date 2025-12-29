9535e55427ea5f4a46204368296aff5701ea3f11
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 07:07:28 -0500

    Definition variable a predire:Fraud_reported

--- src/04_modelisation.R
+++ src/04_modelisation.R
@@ -4,3 +4,7 @@ library (dplyr)
 #Importation des donnees:
 data_transf <- read.csv("data/processed/insurance_final.csv")
 head(data_transf)
+
+#Definition variable a predire
+y <- data_transf$fraud_reported
+head(y)
