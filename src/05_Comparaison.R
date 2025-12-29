b80283deac0c9fb71448284615b4b650abe559c1
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 08:50:54 -0500

    Correction:pas besoin de conversion je me suis corrige

--- src/05_Comparaison.R
+++ src/05_Comparaison.R
@@ -7,19 +7,29 @@ x <- data_transf %>%
   select(age, insured_sex, policy_state, policy_csl,
          incident_type, incident_severity, collision_type,
          authorities_contacted, total_claim_amount)
+
 y <- data_transf$fraud_reported
+
 set.seed(123)
 taille <- nrow(data_transf)
-indice_repartition <- sample(1:taille, size=floor(.8 * taille))
-x_train <- data_transf[indice_repartition, ]
-x_test <- data_transf[-indice_repartition, ]
-y_train <- data_transf[indice_repartition, ]
-y_test <- data_transf[-indice_repartition, ]
+indice_repartition <- sample(1:taille, size = floor(0.8 * taille))
+
+x_train <- x[indice_repartition, ]
+x_test  <- x[-indice_repartition, ]
+
+y_train <- y[indice_repartition]
+y_test  <- y[-indice_repartition]
 print(paste("Dimension des données train: ", 
             paste(dim(x_train), collapse = " x ")))
 print(paste("Dimension des données train: ", 
             paste(dim(x_test), collapse = " x ")))
 
-#Conversion de y_train en valeur binaire 
-y_train_converti <- ifelse(y_train == "Y" | y_train == "1", 1, 0)
 
+# Modèlisation actuarielle avec ces 2 methodes choisies:
+# Régression logistique et Modèle linéaire généralisé (GLM)
+#Je vais justifier un peu plus en bas pourquoi je les ai choisi
+#1. Régression logistique
+
+modele_regLog <- glm(fraud_reported ~ ., 
+                     data = cbind(x_train, fraud_reported = y_train), 
+                     family = binomial())
