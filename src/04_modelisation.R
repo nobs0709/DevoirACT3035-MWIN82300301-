81d74e36d67ea6ef4332d9259e6dda0bb7564cc9
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 07:15:26 -0500

    Definition des variables explications et Objectif de la modelisation

--- src/04_modelisation.R
+++ src/04_modelisation.R
@@ -8,3 +8,22 @@ head(data_transf)
 #Definition variable a predire
 y <- data_transf$fraud_reported
 head(y)
+
+#Je fais ce choix par logique, on a besoin de prédire si une réclamation
+#est frauduleuse. Je trouve que les autres variables sont des causes 
+#potentielles
+
+#Definition variables explicatives
+x <- data_transf %>%
+  select(age, insured_sex, policy_state, policy_csl,
+         incident_type, incident_severity, collision_type,
+         authorities_contacted, total_claim_amount)
+head(x)
+
+# J'ai choisi ces variables parce qu'elles décrivent le profil de l’assuré ainsi que 
+# les caractéristiques du contrat et le type d’incident; exactement ce dont on a besoin 
+# pour determiner le montant de reclamation
+
+#L’objectif de la modélisation est d’identifier 
+#et de prédire les réclamations frauduleuses 
+#à l’aide des informations disponibles sur les sinistres et les assurés.
