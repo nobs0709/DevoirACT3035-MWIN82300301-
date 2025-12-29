0d01f23f7b47801d32161c7697c9683dcdcf5891
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 07:45:27 -0500

    Correction: Preparation des echantillons en faisant la separation en ensembles d'entraînement et de test

--- src/05_Comparaison.R
+++ src/05_Comparaison.R
@@ -3,8 +3,19 @@ library (tidyverse)
 library (dplyr)
 
 #Séparation éventuelle en ensembles d'entraînement et de test
+x <- data_transf %>%
+  select(age, insured_sex, policy_state, policy_csl,
+         incident_type, incident_severity, collision_type,
+         authorities_contacted, total_claim_amount)
+y <- data_transf$fraud_reported
 set.seed(123)
-taille <- nrow(data_selection)
+taille <- nrow(data_transf)
 indice_repartition <- sample(1:taille, size=floor(.8 * taille))
-data_train <- data_selection[indice_repartition, ]
-data_test <- data_selection[-indice_repartition, ]
+x_train <- data_transf[indice_repartition, ]
+x_test <- data_transf[-indice_repartition, ]
+y_train <- data_transf[indice_repartition, ]
+y_test <- data_transf[-indice_repartition, ]
+print(paste("Dimension des données train: ", 
+            paste(dim(x_train), collapse = " x ")))
+print(paste("Dimension des données train: ", 
+            paste(dim(x_test), collapse = " x ")))
