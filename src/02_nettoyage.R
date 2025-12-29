5546a930bc0900c2b391054d41a904ad61f41f5f
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 01:45:43 -0500

    Verification de notre data avec les variables selectionnées

--- src/02_nettoyage.R
+++ src/02_nettoyage.R
@@ -3,3 +3,20 @@ library(tidyverse)
 
 #Je vais faire une selection des variables necessaires
 #pour mon analyse 
+data_selection <- data %>%
+  select(
+    age,
+    insured_sex,
+    policy_state,
+    policy_csl,
+    incident_type,
+    incident_severity,
+    collision_type,
+    authorities_contacted,
+    total_claim_amount,
+    fraud_reported
+  )
+
+# Je vais voir si j'ai correctement mon data
+# avec les variables selectionnées:
+view (data_selection)
