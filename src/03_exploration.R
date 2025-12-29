4e3459baa2d0f805b2ebbecd8a21895793e379d8
nobs0709 <santiananobellamwite@gmail.com>
2025-12-29 05:06:54 -0500

    Tableau des frequences pour les variables categorielles

--- src/03_exploration.R
+++ src/03_exploration.R
@@ -11,3 +11,8 @@ summary (data_transf)
 #2.Mesures de dispersion pour le montant des réclamations
 mean_reclam <- mean(data_transf$total_claim_amount)
 sd_reclam <- sd(data_transf$total_claim_amount)
+# 3. Tableaux de fréquences pour les variables catégorielles
+table(data_transf$incident_severity)
+table(data_transf$insured_sex)
+# Proportion de fraude
+prop.table(table(data_transf$fraud_reported))
